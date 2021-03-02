6f76934ecb7585f75d24d02823ab88141e9345a2
Sebastien Varlet <sebastien.varlet@gmail.com>
2021-03-02 17:16:45 +0000

    It persists an item.
    Co-authored-by: Steve Alexander <SteveAlexander@users.noreply.github.com>

--- test/create_item_test.dart
+++ test/create_item_test.dart
@@ -1,11 +1,50 @@
 import 'package:test/test.dart';
+import 'package:mocktail/mocktail.dart';
 
 class Item {
-  Item(String description);
+  final String description;
+  Item(this.description);
+}
+
+abstract class ItemStore {
+  Future<void> save(Item item);
+}
+
+class CreateItem {
+  final ItemStore itemStore;
+
+  CreateItem(this.itemStore);
+
+  Future<void> create(Item item) {
+    return itemStore.save(item);
+  }
 }
 
 void main() {
   test('it is possible to create an item with a description', () {
     Item('description');
   });
+
+  test('it persists the item', () async {
+    final description = 'Buy some milk';
+    final itemStore = ItemStoreDouble();
+    final interactor = CreateItem(itemStore);
+
+    when(itemStore).calls(#save).thenAnswer((_) => Future(() {
+          return;
+        }));
+
+    await interactor.create(Item(description));
+
+    final callsToSave = verify(itemStore)
+        .called(#save)
+        .withArgs(positional: [captureAny]).captured;
+    final argsToOnlyCallToSave = callsToSave.single;
+    final capturedItem = argsToOnlyCallToSave.single;
+
+    expect(capturedItem, isA<Item>());
+    expect(capturedItem.description, description);
+  });
 }
+
+class ItemStoreDouble extends Mock implements ItemStore {}
