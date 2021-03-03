e1dd0b3ed3b7f616a0b7db69cb32cf68bfeef5f4
Sebastien Varlet <sebastien.varlet@gmail.com>
2021-03-03 11:19:24 +0000

    We can save one item in an InMemoryItemStore

---
+++ test/save_item_test.dart
@@ -0,0 +1,31 @@
+import 'package:test/test.dart';
+
+import 'create_item_test.dart';
+
+void main() {
+  test('it persists the item', () async {
+    final itemStore = InMemoryItemStore();
+    final description = 'Buy some peanut butter';
+    final instant = DateTime.utc(2021, 2, 18, 16, 45, 59);
+    final item = Item(description, ctime: instant);
+
+    await itemStore.save(item);
+
+    expect(
+        itemStore.toList().single,
+        isA<Item>()
+            .having((item) => item.description, 'description', description)
+            .having((item) => item.ctime.isAtSameMomentAs(instant),
+                'created time', isTrue));
+  });
+}
+
+class InMemoryItemStore {
+  final _items = <Item>[];
+
+  Future<void> save(Item item) async {
+    _items.add(item);
+  }
+
+  List<Item> toList() => [..._items];
+}
