e6cf25c2fc1d7f409c4f36f7e22a9a904d563bd6
Sebastien Varlet <sebastien.varlet@gmail.com>
2021-03-03 11:51:44 +0000

    The interactor can list all items when there is one.

--- test/list_items_test.dart
+++ test/list_items_test.dart
@@ -12,7 +12,7 @@ class ListItemsInteractor {
   ListItemsInteractor(this._itemCollection);
 
   Future<List<Item>> listItems() async {
-    return [];
+    return _itemCollection.all();
   }
 }
 
@@ -22,6 +22,23 @@ void main() {
     when(itemStore).calls(#all).thenAnswer((_) => Future(() => <Item>[]));
     final interactor = ListItemsInteractor(itemStore);
     expect(interactor.listItems(), completion(isEmpty));
+  });
+
+  test('one item', () {
+    final description = '::irrelevant description::';
+    final instant = DateTime.utc(2021, 2, 18, 16, 45, 59);
+    final itemCollection = ItemStoreMock();
+    final item = Item(description, ctime: instant);
+    when(itemCollection).calls(#all).thenAnswer((_) => Future(() => [item]));
+    final interactor = ListItemsInteractor(itemCollection);
+    expect(
+        interactor.listItems(),
+        completion(allOf(
+            hasLength(1),
+            contains(isA<Item>()
+                .having((item) => item.description, 'description', description)
+                .having((item) => item.ctime.isAtSameMomentAs(instant), 'ctime',
+                    isTrue)))));
   });
 }
 
