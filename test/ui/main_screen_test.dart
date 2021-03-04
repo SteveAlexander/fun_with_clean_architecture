4a60e12499bf365a65e7b983dd4ad02c645b4cb7
Steve Alexander <steve@stevea.com>
2021-03-04 16:51:10 +0100

    refactor tests

--- test/ui/main_screen_test.dart
+++ test/ui/main_screen_test.dart
@@ -5,7 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mocktail/mocktail.dart';
@@ -16,57 +17,75 @@ import 'package:fun_with_clean_architecture/entities.dart';
 import 'package:fun_with_clean_architecture/ui/providers.dart';
 import 'package:fun_with_clean_architecture/ui/todo_list.dart';
 
-class MockUiProvider extends Mock implements UiProviders {}
+class MockUiProviders extends Mock implements UiProviders {}
 
-void main() {
-  final provider = MockUiProvider();
-  when(provider).calls(#allItems).thenReturn(
-    [
-      Item((b) => b
-        ..description = 'first item'
-        ..ctime = DateTime(2020, 1, 1)),
-    ],
-  );
-  when(provider).calls(#itemsUpdateStream).thenReturn(
-        Stream.fromIterable(
-          <List<Item>>[
-            [
-              Item((b) => b
-                ..description = 'some item'
-                ..ctime = DateTime(2020, 1, 1)),
-            ],
-          ],
-        ),
-      );
-
-  testWidgets('home screen initial state', (WidgetTester tester) async {
-    // Wire up back end stuff
-    await tester.pumpWidget(
+extension SetUpProviderScope on WidgetTester {
+  Future<void> pumpWithScope(Widget widget, UiProviders provider) {
+    return pumpWidget(
       ProviderScope(
         overrides: [providers.overrideWithValue(provider)],
-        child: MyApp(),
+        child: widget,
       ),
     );
+  }
+}
+
+Stream<List<Item>> emptyItemStream() => Stream.fromIterable([]);
+
+void main() {
+  testWidgets('home screen has correct title', (WidgetTester tester) async {
+    final provider = MockUiProviders();
+    when(provider).calls(#itemsUpdateStream).thenReturn(emptyItemStream());
+    await tester.pumpWithScope(MyApp(), provider);
     expect(find.text('Todos'), findsOneWidget);
-    expect(find.byType(NoDataWidget), findsOneWidget);
-    await tester.pump();
-    expect(find.text('some item'), findsOneWidget);
-
-    // NEXT: refactor tests, extend tests to add an item
-
-    // expect(find.text('Buy peanut butter'), findsOneWidget);
-    // expect(find.text('Sell Ascential shares'), findsOneWidget);
-
-    // // Verify that our counter starts at 0.
-    // expect(find.text('A'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
   });
+
+  testWidgets('Before showing the list of items, show nothing',
+      (WidgetTester tester) async {
+    final provider = MockUiProviders();
+    when(provider).calls(#itemsUpdateStream).thenReturn(emptyItemStream());
+    await tester.pumpWithScope(TodoList(), provider);
+    expect(find.byType(NoDataWidget), findsOneWidget);
+  });
+
+  //     [
+  //       [
+  //         Item((b) => b
+  //           ..description = 'some item'
+  //           ..ctime = DateTime(2020, 1, 1)),
+  //       ],
+  //     ],
+  //   ),
+  // );
+
+  // testWidgets('home screen initial state', (WidgetTester tester) async {
+  //   // Wire up back end stuff
+  //   await tester.pumpWidget(
+  //     ProviderScope(
+  //       overrides: [providers.overrideWithValue(provider)],
+  //       child: MyApp(),
+  //     ),
+  //   );
+  //   expect(find.text('Todos'), findsOneWidget);
+  //   expect(find.byType(NoDataWidget), findsOneWidget);
+  //   await tester.pump();
+  //   expect(find.text('some item'), findsOneWidget);
+
+  // NEXT: refactor tests, extend tests to add an item
+
+  // expect(find.text('Buy peanut butter'), findsOneWidget);
+  // expect(find.text('Sell Ascential shares'), findsOneWidget);
+
+  // // Verify that our counter starts at 0.
+  // expect(find.text('A'), findsOneWidget);
+  // expect(find.text('1'), findsNothing);
+
+  // // Tap the '+' icon and trigger a frame.
+  // await tester.tap(find.byIcon(Icons.add));
+  // await tester.pump();
+
+  // // Verify that our counter has incremented.
+  // expect(find.text('0'), findsNothing);
+  // expect(find.text('1'), findsOneWidget);
+  // });
 }
